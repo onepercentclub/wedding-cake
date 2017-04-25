aef51e08a8396bee03725125d63d2ef83f1cb256
Maurice Kleine <info@mauricekleine.nl>
2017-04-25 18:16:17 +0200

    dynamic api endpoint

--- src/ui/components/wedding-cake/component.ts
+++ src/ui/components/wedding-cake/component.ts
@@ -1,5 +1,7 @@
 import Component, { tracked } from "@glimmer/component";
 
+import { endpoint } from '../../../utils/settings';
+
 export default class WeddingCake extends Component {
     @tracked engagementNumber: number = 0;
 
@@ -11,7 +13,7 @@ export default class WeddingCake extends Component {
     }
 
     async getEngagementNumber() {
-        const response = await fetch('http://onepercent.localhost:8000/api/analytics/engagement-number');
+        const response = await fetch(endpoint);
         const json = await response.json();
         this.engagementNumber = json.engagement_number;
     }
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,3 +1,9 @@
+const ENV = { environment: 'development' }; // this should be imported
+
+export const endpoint: string = ENV.environment === 'production'
+    ? 'http://onepercent.localhost:8000/api/analytics/engagement-number'
+    : 'http://onepercent.localhost:8000/api/analytics/engagement-number';
+
 export const milestones: number[] = [
     11220,
     13720,
