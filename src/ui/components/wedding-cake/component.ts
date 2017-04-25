284ea77243fdbb11b72078cbbd2845e14259929a
Maurice Kleine <info@mauricekleine.nl>
2017-04-25 13:33:13 +0200

    feat: dynamic update of engagementnumber and weighted engagementnumber in bar

--- src/ui/components/wedding-cake/component.ts
+++ src/ui/components/wedding-cake/component.ts
@@ -2,4 +2,10 @@ import Component, { tracked } from "@glimmer/component";
 
 export default class WeddingCake extends Component {
     @tracked engagementNumber: number = 200;
+
+    didInsertElement() {
+        setInterval(() => {
+            this.engagementNumber = this.engagementNumber + 100;
+        }, 1000);
+    }
 }
--- src/ui/components/wedding-cake/engagement-graph/component.ts
+++ src/ui/components/wedding-cake/engagement-graph/component.ts
@@ -1,5 +1,17 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class EngagementGraph extends Component {
+    @tracked weightedEngagementNumber: number = 0;
 
+    didInsertElement() {
+        this.updateWeightedEngagementNumber();
+    }
+
+    didUpdate() {
+        this.updateWeightedEngagementNumber();
+    }
+
+    updateWeightedEngagementNumber() {
+        return this.weightedEngagementNumber = Math.round((this.args.engagementNumber / 20000) * 100);
+    }
 }; 
