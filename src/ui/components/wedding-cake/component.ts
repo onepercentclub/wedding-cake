6685d6df56c7fa101457582c39ead28cb81f3553
Maurice Kleine <info@mauricekleine.nl>
2017-05-10 13:36:15 +0200

    fix function binding

--- src/ui/components/wedding-cake/component.ts
+++ src/ui/components/wedding-cake/component.ts
@@ -3,18 +3,18 @@ import Component, { tracked } from "@glimmer/component";
 import { endpoint } from '../../../utils/settings';
 
 export default class WeddingCake extends Component {
-    @tracked engagementNumber: number = 3200;
+    @tracked engagementNumber: number;
 
     didInsertElement() {
-        this.getEngagementNumber();
+      this.getEngagementNumber();
 
-        // refresh the number every 10 minutes
-        setInterval(this.getEngagementNumber, 1000 * 60 * 10);
+      // refresh the number every 10 minutes
+      setInterval(this.getEngagementNumber.bind(this), 1000 * 60 * 10);
     }
 
     async getEngagementNumber() {
-        const response = await fetch(endpoint);
-        const json = await response.json();
-        this.engagementNumber = json.results[0].series[0].values[0][1];
+      const response = await fetch(endpoint);
+      const json = await response.json();
+      this.engagementNumber = json.results[0].series[0].values[0][1];
     }
 };
